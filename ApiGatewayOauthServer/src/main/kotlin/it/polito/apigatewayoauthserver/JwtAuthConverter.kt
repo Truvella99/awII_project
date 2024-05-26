9ebd9d153028fd4f9a23c4e92269573467040431
Truvella99 <gagliardo9975@gmail.com>
2024-05-26 17:09:39 +0200

    add jwt roles handling

---
+++ ApiGatewayOauthServer/src/main/kotlin/it/polito/apigatewayoauthserver/JwtAuthConverter.kt
@@ -0,0 +1,40 @@
+package it.polito.apigatewayoauthserver
+
+import org.springframework.core.convert.converter.Converter
+import org.springframework.security.authentication.AbstractAuthenticationToken
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.security.oauth2.jwt.JwtClaimNames
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken
+import org.springframework.stereotype.Component
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter
+import org.springframework.security.oauth2.jwt.Jwt
+
+@Component
+class JwtAuthConverter(
+    private val properties: JwtAuthConverterProperties
+) : Converter<Jwt, AbstractAuthenticationToken> {
+
+    private val jwtGrantedAuthoritiesConverter = JwtGrantedAuthoritiesConverter()
+
+    override fun convert(jwt: Jwt): AbstractAuthenticationToken {
+        val authorities = (jwtGrantedAuthoritiesConverter.convert(jwt)?.toSet() ?: emptySet<GrantedAuthority>()) +
+                extractResourceRoles(jwt)
+        return JwtAuthenticationToken(jwt, authorities, getPrincipalClaimName(jwt))
+    }
+
+    private fun getPrincipalClaimName(jwt: Jwt): String {
+        val claimName = properties.principalAttribute ?: JwtClaimNames.SUB
+        return jwt.getClaim(claimName) ?: ""
+    }
+
+    private fun extractResourceRoles(jwt: Jwt): Collection<GrantedAuthority> {
+        return try {
+            val resourceAccess = jwt.getClaim<Map<String, Any>>("realm_access")
+            val resourceRoles = resourceAccess?.get("roles") as? Collection<String>
+            resourceRoles?.map { role -> SimpleGrantedAuthority("ROLE_$role") }?.toSet() ?: emptySet()
+        } catch (e: Exception) {
+            emptySet()
+        }
+    }
+}
--- ApiGatewayOauthServer/src/main/kotlin/it/polito/apigatewayoauthserver/SecurityConfig.kt
+++ ApiGatewayOauthServer/src/main/kotlin/it/polito/apigatewayoauthserver/SecurityConfig.kt
@@ -2,22 +2,35 @@ package it.polito.apigatewayoauthserver
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
-import org.springframework.security.config.web.server.ServerHttpSecurity
 import org.springframework.security.web.SecurityFilterChain
 
 
 @Configuration
-class SecurityConfig() {
+@EnableWebSecurity
+@EnableMethodSecurity
+class SecurityConfig(
+    private val jwtAuthConverterProperties: JwtAuthConverterProperties
+) {
+
     @Bean
-    fun filterChain(httpSecurity: HttpSecurity): SecurityFilterChain {
+    fun jwtAuthConverter(): JwtAuthConverter {
+        return JwtAuthConverter(jwtAuthConverterProperties)
+    }
+
+    @Bean
+    fun filterChain(httpSecurity: HttpSecurity, jwtAuthConverter: JwtAuthConverter): SecurityFilterChain {
         return httpSecurity
             .authorizeHttpRequests {
                 it.requestMatchers("/").permitAll()
                 it.anyRequest().authenticated()
             }
-            .oauth2ResourceServer { it.jwt {  } }
+            .oauth2ResourceServer {
+                it.jwt { jwtConfigurer -> jwtConfigurer.jwtAuthenticationConverter(jwtAuthConverter) }
+            }
             .sessionManagement {
                 it.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             }
