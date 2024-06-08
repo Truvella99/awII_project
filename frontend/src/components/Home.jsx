3867605b3b0f2c7e33d7ce608185631b41e78675
Truvella99 <gagliardo9975@gmail.com>
2024-06-08 21:23:12 +0200

    add User Role for all microservices

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -2,7 +2,9 @@ import React, { useEffect, useState } from 'react';
 import { Form, Button, Container, Row, Col } from 'react-bootstrap';
 function Home({ me }) {
     const [data, setData] = useState('');
-    const [result, setResult] = useState(null);
+    const [crmResult, setCrmResult] = useState(null);
+    const [cmResult, setCmResult] = useState(null);
+    const [docStoreresult, setDocStoreResult] = useState(null);
 
     useEffect(() => {
         const postData = async () => {
@@ -15,9 +17,35 @@ function Home({ me }) {
                     }
                 });
                 const result = await res.json();
-                setResult(result);
+                setCrmResult(result);
             } catch (error) {
-                setResult({});
+                setCrmResult({});
+            }
+            try {
+                const res = await fetch('communicationManager/data', {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'X-XSRF-TOKEN': me?.xsrfToken,
+                    }
+                });
+                const result = await res.json();
+                setCmResult(result);
+            } catch (error) {
+                setCmResult({});
+            }
+            try {
+                const res = await fetch('docStore/data', {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'X-XSRF-TOKEN': me?.xsrfToken,
+                    }
+                });
+                const result = await res.json();
+                setDocStoreResult(result);
+            } catch (error) {
+                setDocStoreResult({});
             }
         };
         postData().then();
@@ -30,8 +58,12 @@ function Home({ me }) {
                         <div style={{ border: 'dashed 1px gray', padding: '1em' }}>
                             <p>User Infos:</p>
                             <pre>{JSON.stringify(me.principal.userInfo,null,4)}</pre>
-                            <p>User Role (run CRM microservice to see):</p>
-                            <pre>{result? JSON.stringify(result["roles"],null,4): null}</pre>
+                            <p>User Role (Customer Relationships Management Microservice):</p>
+                            <pre>{crmResult? JSON.stringify(crmResult["roles"],null,4): null}</pre>
+                            <p>User Role (Communication Manager Microservice):</p>
+                            <pre>{cmResult? JSON.stringify(cmResult["roles"],null,4): null}</pre>
+                            <p>User Role (Document Store Microservice):</p>
+                            <pre>{docStoreresult? JSON.stringify(docStoreresult["roles"],null,4): null}</pre>
                         </div>
                     </Col>
                 </Row>
