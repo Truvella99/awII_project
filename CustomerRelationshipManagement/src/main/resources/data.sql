9876b3dea7380883834b779758d1530fdbde6394
Truvella99 <gagliardo9975@gmail.com>
2024-12-15 15:43:13 +0100

    add crm db prepopulation logic

---
+++ CustomerRelationshipManagement/src/main/resources/data.sql
@@ -0,0 +1,10 @@
+-- Begin the transaction
+BEGIN;
+
+INSERT INTO contact (id, name, surname, ssncode, category, customer_id, professional_id)
+VALUES (1, 'John', 'Doe', NULL, NULL, NULL, NULL);
+
+-- If any error occurs, rollback the transaction
+-- Use ROLLBACK instead of COMMIT to undo all changes in case of an error
+-- In PostgreSQL, rollback happens automatically if there's an error during execution
+COMMIT;
